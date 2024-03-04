2d30a7ed7ac14c1af6ba5c2a7e1a2be1bce2a05b
Me7eorite <sca7tered@outlook.com>
2024-03-05 01:25:06 +0800

    test

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,16 +1,16 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'NoleBase'
+export const siteShortName = 'NoleBase'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/Me7eorite'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'me7eorite.github.io'
 export const targetDomain = `https://${plainTargetDomain}`
 
 /** 社交媒体 */
